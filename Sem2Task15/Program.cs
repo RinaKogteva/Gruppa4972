1b3ad510652a358e0cba85b0c90b2cffc7cb4112
RinaKogteva <ledbina@gmail.com>
2023-10-01 15:12:56 +0300

    Семинар 2 + домашнее задание

---
+++ Sem2Task15/Program.cs
@@ -0,0 +1,22 @@
+﻿// Домашнее задание к семинару 2. Задача №15.
+// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+// * Сделать вариант с использованием конструкции Dictionary
+
+
+Console.Write("Введи цифру, обозначающую день недели: ");
+int dayNumber = Convert.ToInt32(Console.ReadLine());
+
+void CheckingTheDayOfTheWeek (int dayNumber) {
+  if (dayNumber == 6 || dayNumber == 7) {
+  Console.WriteLine("-> да, этот день выходной");
+  }
+  else if (dayNumber < 1 || dayNumber > 7) {
+    Console.WriteLine("это не день недели");
+  }
+  else Console.WriteLine(" -> нет, этот день не выходной");
+}
+
+CheckingTheDayOfTheWeek(dayNumber);
