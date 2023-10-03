034ede42abb59ce9aa1e60ecc2d8df771698002a
RinaKogteva <ledbina@gmail.com>
2023-10-03 20:53:51 +0300

    ДЗ Семинар 3 автотесты

--- Sem3Task19/Program.cs
+++ Sem3Task19/Program.cs
@@ -2,46 +2,96 @@
 // является ли оно палиндромом. Сделать через словарь 
 
 // Вывод сообщения и запись полученного числа
-bool Prompt(string message)
-{
-    Console.WriteLine(message);
-    string value = Console.ReadLine()??",";
-    int number = Convert.ToInt32(value);
+// bool Prompt(string message)
+// {
+//     Console.WriteLine(message);
+//     string value = Console.ReadLine()??",";
+//     int number = Convert.ToInt32(value);
 
-    if (number < 10000 || number > 99999) 
-    {
-        Console.WriteLine("Число не пятизначное");
-        return false;
+//     if (number < 10000 || number > 99999) 
+//     {
+//         Console.WriteLine("Число не пятизначное");
+//         return false;
+//     }
+
+//     Dictionary<int, int> numberDictionary = NumberToDictionary(number);
+
+//     return CheckingPalindrome(numberDictionary);
+// }
+
+// // Запись цифр числа в словарь в обратном порядке
+// Dictionary<int,int> NumberToDictionary(int number)
+// {
+//     Dictionary<int, int> nums = new Dictionary<int, int>();
+//     for (int i = 5; i >= 1; i--)
+//     {
+//         nums.Add(i, number % 10);
+//         number = number / 10;
+//     }
+
+//     return nums;
+// }
+
+// //12321 - > 1221
+// //23132 - > 2332
+
+// // Проверяет, является ли число палиндромом
+// bool CheckingPalindrome(Dictionary<int, int> numberDictionary)
+// {
+//     if (numberDictionary[5] == numberDictionary[1] && numberDictionary[4] == numberDictionary[2])
+//         return true;
+//     else return false;
+// }
+
+
+// Console.WriteLine(Prompt("Введите число: "));
+
+
+using System;
+
+public class Answer
+{
+    static bool IsPalindrome(int number){
+      // Введите свое решение ниже
+       if (number < 10000 || number > 99999)
+        {
+            Console.WriteLine("Число не пятизначное");
+            return false;
+        }
+
+        int originalNumber = number;
+        int reverseNumber = 0;
+
+        while (number > 0)
+        {
+            int digit = number % 10;
+            reverseNumber = reverseNumber * 10 + digit;
+            number = number / 10;
+        }
+
+        if (originalNumber == reverseNumber)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
     }
+  
+  // Не удаляйте и не меняйте метод Main! 
+      static public void Main(string[] args) {
+        int number;
 
-    Dictionary<int, int> numberDictionary = NumberToDictionary(number);
+        if (args.Length >= 1) {
+            number = int.Parse(args[0]);
+        } else {
+           // Здесь вы можете поменять значения для отправки кода на Выполнение
+            number = 234322;
+        }
 
-    return CheckingPalindrome(numberDictionary);
-}
-
-// Запись цифр числа в словарь в обратном порядке
-Dictionary<int,int> NumberToDictionary(int number)
-{
-    Dictionary<int, int> nums = new Dictionary<int, int>();
-    for (int i = 5; i >= 1; i--)
-    {
-        nums.Add(i, number % 10);
-        number = number / 10;
+        // Не удаляйте строки ниже
+        bool result = IsPalindrome(number);
+        System.Console.WriteLine($"{result}");
     }
-
-    return nums;
 }
-
-//12321 - > 1221
-//23132 - > 2332
-
-// Проверяет, является ли число палиндромом
-bool CheckingPalindrome(Dictionary<int, int> numberDictionary)
-{
-    if (numberDictionary[5] == numberDictionary[1] && numberDictionary[4] == numberDictionary[2])
-        return true;
-    else return false;
-}
-
-
-Console.WriteLine(Prompt("Введите число: "));
--- Sem3Task21/Program.cs
+++ Sem3Task21/Program.cs
@@ -2,30 +2,51 @@
 // Осуществить возможность ввода всех точек в одну строку
 
 // Вывод сообщения и вызов метода DistanceCalculation
-double Prompt(string message1)
+// 
+
+
+// Внутри класса Answer напишите метод DistanceBetweenPoints, который принимает на вход координаты двух точек pointA и pointB 
+// в виде массива целых чисел, и возвращает расстояние между ними в 3D пространстве.
+
+
+using System;
+
+public class Answer
 {
-    Console.WriteLine(message1);
-    string values = Console.ReadLine()??",";
+    private static double Length(int[] pointA, int[] pointB)
+    {
+      // Введите свое решение ниже
+      
+       var xPow = Math.Pow(pointB[0] - pointA[0],2);
+       var yPow = Math.Pow(pointB[1] - pointA[1],2);
+       var zPow = Math.Pow(pointB[2] - pointA[2],2);
+       return Math.Sqrt(xPow + yPow + zPow);
 
-    string[] valuesArray = values.Split(",");
-    int xA = Convert.ToInt32(valuesArray[0]);
-    int yA = Convert.ToInt32(valuesArray[1]);
-    int zA = Convert.ToInt32(valuesArray[2]);
-    int xB = Convert.ToInt32(valuesArray[3]);
-    int yB = Convert.ToInt32(valuesArray[4]);
-    int zB = Convert.ToInt32(valuesArray[5]);
+    }
 
-    return DistanceCalculation(xA, yA, zA, xB, yB, zB);
+  // Не удаляйте и не меняйте метод Main! 
+    public static void Main(string[] args) {
+        int x1, x2, x3, y1, y2, y3;
+
+        if (args.Length >= 6) {
+            x1 = int.Parse(args[0]);
+            x2 = int.Parse(args[1]);
+            x3 = int.Parse(args[2]);
+            y1 = int.Parse(args[3]);
+            y2 = int.Parse(args[4]);
+            y3 = int.Parse(args[5]);
+        } else {
+           // Здесь вы можете поменять значения для отправки кода на Выполнение
+            x1 = 3;
+            x2 = 6;
+            x3 = 8;
+            y1 = 2;
+            y2 = 1;
+            y3 = -7;
+        }
+
+        // Не удаляйте строки ниже
+        double result = Length(new int[]{x1, x2, x3}, new int[]{y1, y2, y3});
+        Console.WriteLine($"{result:F2}");
+    }
 }
-
-
-//A (3,6,8); B (2,1,-7)
-// Расчёт расстояния
-double DistanceCalculation(int xA, int yA, int zA, int xB, int yB, int zB)
-{
-    return Math.Sqrt(Math.Pow(Math.Abs(xA - xB), 2)
-                    + Math.Pow(Math.Abs(yA - yB), 2)
-                    + Math.Pow(Math.Abs(zA - zB), 2));
-}
-
-Console.WriteLine(Prompt("Введите 6 чисел через запятую (xA,yA,zA,xB,yB,zB): "));
