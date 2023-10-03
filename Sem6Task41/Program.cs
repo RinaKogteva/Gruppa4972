5671ec2f7dee945d880d1a2321ad263b9ab960df
RinaKogteva <ledbina@gmail.com>
2023-10-03 21:09:42 +0300

    ДЗ семинар 6

--- Sem6Task41/Program.cs
+++ Sem6Task41/Program.cs
@@ -7,56 +7,86 @@
 
 //                                                 Тело программы
 //------------------------------------------------------------------------------------------------------------------
-using System.Text.RegularExpressions;
-Console.Clear();
+// using System.Text.RegularExpressions;
+// Console.Clear();
 
-int numOfKeyPresses = ReadInt("Введите количество нажатий: ");  // Ввод пользователем количества нажатий.
-string data = KeyPresses("Введите строку: ", numOfKeyPresses);  // Получение нажатий с клавиатуры.
-//Console.WriteLine(data);
-int count = CountPositiveNumbers(data);                         // Подсчет положительных чисел в строке.
-PrintResult($"\nВведено чисел больше нуля: {count}");           // Вывод результата в консоль.
+// int numOfKeyPresses = ReadInt("Введите количество нажатий: ");  // Ввод пользователем количества нажатий.
+// string data = KeyPresses("Введите строку: ", numOfKeyPresses);  // Получение нажатий с клавиатуры.
+// //Console.WriteLine(data);
+// int count = CountPositiveNumbers(data);                         // Подсчет положительных чисел в строке.
+// PrintResult($"\nВведено чисел больше нуля: {count}");           // Вывод результата в консоль.
 
-//                                                     Методы
-//------------------------------------------------------------------------------------------------------------------
+// //                                                     Методы
+// //------------------------------------------------------------------------------------------------------------------
 
-// Ввод целого числа с клавиатуры.
-int ReadInt(string msg)
-{
-    Console.Write(msg);
-    return int.Parse(Console.ReadLine() ?? "0");
-}
+// // Ввод целого числа с клавиатуры.
+// int ReadInt(string msg)
+// {
+//     Console.Write(msg);
+//     return int.Parse(Console.ReadLine() ?? "0");
+// }
 
-// Получение нажатий с клавиатуры.
-string KeyPresses(string msg, int numOfKeyPresses)
-{
-    Console.Write(msg);
-    string data = string.Empty;
+// // Получение нажатий с клавиатуры.
+// string KeyPresses(string msg, int numOfKeyPresses)
+// {
+//     Console.Write(msg);
+//     string data = string.Empty;
     
-    for (int i = 0; i < numOfKeyPresses; i++)
-    {
-        var keyInfo = Console.ReadKey();
-        data += keyInfo.KeyChar;
-    }
+//     for (int i = 0; i < numOfKeyPresses; i++)
+//     {
+//         var keyInfo = Console.ReadKey();
+//         data += keyInfo.KeyChar;
+//     }
 
-    return data;
-}
+//     return data;
+// }
 
-// Подсчет положительных чисел в строке с использованием регулярных выражений.
-int CountPositiveNumbers(string input)
+// // Подсчет положительных чисел в строке с использованием регулярных выражений.
+// int CountPositiveNumbers(string input)
+// {
+//     string pattern = @"[-]?[0-9]+";
+//     MatchCollection matches = Regex.Matches(input, pattern);
+
+//     int count = 0;
+//     foreach (Match match in matches)
+//     {
+//         if (int.Parse(match.Value) > 0) { count++; }
+//     }
+//     return count;
+// }
+
+// // Вывод результата в консоль.
+// void PrintResult(string msg)
+// {
+//     Console.WriteLine(msg);
+// }
+
+Console.Write("Введите количество элементов массива: ");
+int m = Convert.ToInt32(Console.ReadLine());
+int[] Array = new int[m];
+
+void mas(int m)
 {
-    string pattern = @"[-]?[0-9]+";
-    MatchCollection matches = Regex.Matches(input, pattern);
-
-    int count = 0;
-    foreach (Match match in matches)
-    {
-        if (int.Parse(match.Value) > 0) { count++; }
-    }
-    return count;
-}
-
-// Вывод результата в консоль.
-void PrintResult(string msg)
+for (int i = 0; i < m; i++)
 {
-    Console.WriteLine(msg);
+Console.WriteLine($"Введите {i+1} элемент массива ");
+Array[i] = Convert.ToInt32(Console.ReadLine());
 }
+
+}
+
+int kol(int[] Array)
+{
+int i=0;
+int sum = 0;
+while (i < Array.Length)
+{
+if(Array[i]>0)
+sum = sum + 1;
+i = i + 1;
+}
+return sum;
+}
+
+mas(m);
+Console.Write($"\n Чисел больше нуля: {kol(Array)}");
